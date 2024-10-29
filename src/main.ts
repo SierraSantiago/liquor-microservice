315f5ac86e65e986d6a56b66488f16fced5ad616
SierraSantiago <SantiagoSierra003@gmail.com>
2024-10-29 07:49:42 -0500

    Refactor main.ts to create a microservice and add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule, {
+      transport: Transport.TCP,
+      options: {
+        port: +process.env.MICROSERVICE_PORT,
+      }
+   
+  });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  })); 
+
+  await app.listen();
+  logger.log('Microservice is listeningon port ' + +process.env.MICROSERVICE_PORT);
 }
 bootstrap();
